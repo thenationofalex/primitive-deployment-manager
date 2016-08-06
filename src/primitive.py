35f00eb0601241cf641ec075ceddf72dcc2cdc9d
Alex Best <alexb@thenationofalex.info>
2016-08-07 00:09:02 +1000

    [App] Refactored code to suit PEP8.

--- src/primitive.py
+++ src/primitive.py
@@ -14,8 +14,111 @@ DOTENV_PATH = os.path.dirname(os.path.realpath(__file__)) + '/../.env'
 DOTENV = Dotenv(DOTENV_PATH)
 os.environ.update(DOTENV)
 
+    # Prepare package list
+def prepare_packages(deployment_data, project_name):
+    '''Create list of APT packages to install'''
+    packages = []
+    for package in deployment_data['package']:
+        packages.append(package['name'])
+    packages_to_install = 'mkdir -p deploy/codebase/' + project_name + \
+    ' && mkdir deploy/config && sudo -S apt-get install -y ' + ' '.join(packages)
+    return packages_to_install
+
+def install_packages(ssh, packages_to_install, password):
+    '''Install packages via APT'''
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(packages_to_install),
+                                                         get_pty=True)
+    ssh_stdin.write(password + '\n')
+    ssh_stdin.flush()
+    ssh_stderr.read()
+    ssh_stdout.read()
+
+def deploy_config(sftp, deployment_data, username):
+    '''SFTP Config files'''
+    for config in deployment_data['config']:
+        local_file = os.path.dirname(os.path.realpath(__file__)) + config['template']
+        remote_file = '/home/' + username + config['template']
+        print(local_file)
+        sftp.put(local_file, remote_file)
+
+def deploy_code_base(sftp, deployment_data, username):
+    '''SFTP Codebase'''
+    for code in deployment_data['codebase']:
+        local_folder = os.path.dirname(os.path.realpath(__file__)) + code['dir']
+        remote_folder = '/home/' +  username + code['dir']
+        for filename in os.listdir(local_folder):
+            to_transfer = os.path.dirname(os.path.realpath(__file__)) + \
+            code['dir'] + '/' + filename
+            remote_transfer = remote_folder + '/' + filename
+            print(to_transfer)
+            sftp.put(to_transfer, remote_transfer)
+
+def move_config_files(ssh, deployment_data, username, password):
+    '''Move config files and return site name'''
+    for config in deployment_data['config']:
+        configs_to_move = 'sudo -S cp -r /home/' + username + \
+        config['template'] + ' ' + config['deploy_to']
+
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(configs_to_move),
+                                                         get_pty=True)
+    ssh_stdin.write(password + '\n')
+    ssh_stdin.flush()
+    ssh_stderr.read()
+    ssh_stdout.read()
+
+def set_servername(ssh, deployment_data, ip_address, password):
+    '''Set ServerName in Apache VirtualHost'''
+    for config in deployment_data['config']:
+        if config['service'] == 'webserver':
+            update_server_name = 'sudo -S sed -i \'/.*ServerName.*/c\\ServerName ' + \
+            ip_address + '\' /etc/apache2/sites-available/helloworld.conf'
+
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(update_server_name),
+                                                         get_pty=True)
+    ssh_stdin.write(password + '\n')
+    ssh_stdin.flush()
+    ssh_stderr.read()
+    ssh_stdout.read()
+
+def move_code_base(ssh, deployment_data, username, password):
+    '''Move Codebase'''
+    for code in deployment_data['codebase']:
+        code_to_move = 'sudo -S mkdir -p ' + code['deploy_to'] + \
+        ' && sudo -S cp -r /home/' + username + '/' + code['dir'] + \
+        '/* ' + code['deploy_to']
+
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(code_to_move),
+                                                         get_pty=True)
+    ssh_stdin.write(password + '\n')
+    ssh_stdin.flush()
+    ssh_stderr.read()
+    ssh_stdout.read()
+
+def clean_up_deployment(ssh, username, site, password):
+    '''Clean up deployment directory, remove default apache site,
+    enable new site, restart apache services'''
+    clean_up = 'sudo rm -rf /home/' + username + '/deploy && ' \
+    'sudo -S rm -rf /var/www/html && sudo -S a2ensite ' + site + \
+    ' && sudo -S service apache2 restart'
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(clean_up),
+                                                         get_pty=True)
+    ssh_stdin.write(password + '\n')
+    ssh_stdin.flush()
+    ssh_stderr.read()
+    ssh_stdout.read()
+
+def run_http_test(ip_address):
+    '''Test server http response'''
+    http_test = http.client.HTTPConnection(ip_address)
+    http_test.request("GET", "/")
+    response = http_test.getresponse()
+    data_response = response.read()
+    print(colored('HTTP Test', 'cyan'))
+    print(response.status, response.reason)
+    print(data_response)
+
 def main():
-    '''Main'''
+    '''Start Primitive Deployment Manager'''
     # Load deployment config
     server_password = os.environ.get('SERVER_PASSWORD')
     deployment_data = []
@@ -29,14 +132,6 @@ def main():
 
     for nodes in deployment_data['nodes']:
         print('- ' + nodes['ip'])
-
-    # Prepare package list
-    packages = []
-    for package in deployment_data['package']:
-        packages.append(package['name'])
-
-    packages_to_install = 'mkdir -p deploy/codebase/' + project_name + \
-    ' && mkdir deploy/config && sudo -S apt-get install -y ' + ' '.join(packages)
 
     # Confirm codebase deployment to nodes.
     while True:
@@ -60,13 +155,9 @@ def main():
                     password=server_password, timeout=4)
 
         # Install Packages
-        print(colored('Installing packages: ' + str(packages), 'cyan'))
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(packages_to_install),
-                                                             get_pty=True)
-        ssh_stdin.write(server_password + '\n')
-        ssh_stdin.flush()
-        ssh_stderr.read()
-        ssh_stdout.read()
+        packages_to_install = prepare_packages(deployment_data, project_name)
+        print(colored('Installing packages', 'cyan'))
+        install_packages(ssh, packages_to_install, server_password)
 
         # Start SFTP
         transport = paramiko.Transport((nodes['ip'], 22))
@@ -76,89 +167,35 @@ def main():
 
         # Deploy configs
         print(colored('Copying configs:', 'cyan'))
-        for config in deployment_data['config']:
-            local_file = os.path.dirname(os.path.realpath(__file__)) + config['template']
-            remote_file = '/home/' + nodes['username'] + config['template']
-            print(local_file)
-            sftp.put(local_file, remote_file)
+        deploy_config(sftp, deployment_data, nodes['username'])
 
         # Deploy code base
         print(colored('Copying codebase:', 'cyan'))
-        for code in deployment_data['codebase']:
-            local_folder = os.path.dirname(os.path.realpath(__file__)) + code['dir']
-            remote_folder = '/home/' +  nodes['username'] + code['dir']
-            for filename in os.listdir(local_folder):
-                to_transfer = os.path.dirname(os.path.realpath(__file__)) + \
-                code['dir'] + '/' + filename
-                remote_transfer = remote_folder + '/' + filename
-                print(to_transfer)
-                sftp.put(to_transfer, remote_transfer)
+        deploy_code_base(sftp, deployment_data, nodes['username'])
 
         sftp.close()
         transport.close()
 
         # Move config files.
-        for config in deployment_data['config']:
-            configs_to_move = 'sudo -S cp -r /home/' + nodes['username'] + \
-            config['template'] + ' ' + config['deploy_to']
-            if config['service'] == 'webserver':
-                site_to_enable = config['name']
-
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(configs_to_move),
-                                                             get_pty=True)
-        ssh_stdin.write(server_password + '\n')
-        ssh_stdin.flush()
-        ssh_stderr.read()
-        ssh_stdout.read()
+        move_config_files(ssh, deployment_data, nodes['username'], server_password)
 
         # Set ServerName in Apache conf
-        for config in deployment_data['config']:
-            if config['service'] == 'webserver':
-                update_server_name = 'sudo -S sed -i \'/.*ServerName.*/c\ServerName ' + \
-                nodes['ip'] + '\' /etc/apache2/sites-available/helloworld.conf'
-
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(update_server_name),
-                                                             get_pty=True)
-        ssh_stdin.write(server_password + '\n')
-        ssh_stdin.flush()
-        ssh_stderr.read()
-        ssh_stdout.read()
+        set_servername(ssh, deployment_data, nodes['ip'], server_password)
 
         # Move codebase
-        for code in deployment_data['codebase']:
-            code_to_move = 'sudo -S mkdir -p ' + code['deploy_to'] + \
-            ' && sudo -S cp -r /home/' + nodes['username'] + '/' + code['dir'] + \
-            '/* ' + code['deploy_to']
-
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(code_to_move),
-                                                             get_pty=True)
-        ssh_stdin.write(server_password + '\n')
-        ssh_stdin.flush()
-        ssh_stderr.read()
-        ssh_stdout.read()
+        move_code_base(ssh, deployment_data, nodes['username'], server_password)
 
         # Clean up deployment directory, remove default apache site,
         # enable new site, restart apache services
-        clean_up = 'sudo rm -rf /home/' + nodes['username'] + '/deploy && ' \
-        'sudo -S rm -rf /var/www/html && sudo -S a2ensite ' + site_to_enable + \
-        ' && sudo -S service apache2 restart'
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(str(clean_up),
-                                                             get_pty=True)
-        ssh_stdin.write(server_password + '\n')
-        ssh_stdin.flush()
-        ssh_stderr.read()
-        ssh_stdout.read()
+        for config in deployment_data['config']:
+            if config['service'] == 'webserver':
+                site_to_enable = config['name']
+        clean_up_deployment(ssh, nodes['username'], site_to_enable, server_password)
 
         ssh.close()
 
         #Run HTTP tests
-        http_test = http.client.HTTPConnection(nodes['ip'])
-        http_test.request("GET", "/")
-        response = http_test.getresponse()
-        data_response = response.read()
-        print(colored('HTTP Test', 'cyan'))
-        print(response.status, response.reason)
-        print(data_response)
+        run_http_test(nodes['ip'])
         print(colored('Node deployed', 'magenta'))
 
 if __name__ == '__main__':
